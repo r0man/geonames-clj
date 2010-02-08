93529ac0a76df9344002be014e587148d93e3455
Roman Scherer <roman.scherer@burningswell.com>
2010-02-08 22:08:06 +0100

    Changed version to 0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geonames "0.1.0"
+(defproject geonames "0.1"
   :description "Clojure API for GeoNames."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
