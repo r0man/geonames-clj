50583e08e0f3f840b52ad3b961e802696a1c0a55
Roman Scherer <roman.scherer@burningswell.com>
2010-02-09 14:45:33 +0100

    Added lein-clojars to the development dependencies.

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Clojure API for GeoNames."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.1.0"]])
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.1.0"]])
 
