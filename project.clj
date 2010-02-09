cc4f1f84be5ebe7b4ca9232d56e59fa829972e4f
Roman Scherer <roman.scherer@burningswell.com>
2010-02-09 14:52:56 +0100

    Added github url to project file.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject geonames "0.3"
+(defproject geonames "0.4-SNAPSHOT"
   :description "Clojure API for GeoNames."
+  :url "http://github.com/r0man/geonames-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
