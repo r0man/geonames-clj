d428ed906ef65fc3fbd03dfc2011352b6bfd42de
Roman <roman@burningswell.com>
2011-09-09 12:44:03 +0200

    Fixed region url.

--- src/geonames/regions.clj
+++ src/geonames/regions.clj
@@ -2,12 +2,12 @@
   (:use [clojure.contrib.duck-streams :only (read-lines)]
         [clojure.contrib.string :only (trim)]))
 
-(def *url* "http://download.geonames.org/export/dump/admin1Codes.txt")
+(def *url* "http://download.geonames.org/export/dump/admin1CodesASCII.txt")
 
 (defstruct region :name :country-id :region-id :geonames-id)
 
-(defn parse-region [line] 
-  (if-let [[_ geonames-id country-id region-id name] (re-find #"((..)\.([^\t]+))\t(.+)" line)]    
+(defn parse-region [line]
+  (if-let [[_ geonames-id country-id region-id name] (re-find #"((..)\.([^\t]+))\t(.+)" line)]
     (struct region (trim name) (trim country-id) (trim region-id) (trim geonames-id))))
 
 (defn parse-regions
