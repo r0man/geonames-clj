f11e64e174ccec0c4749cd5135ac636efee2fb27
Roman <roman@burningswell.com>
2011-09-09 12:43:50 +0200

    Upgraded to clojure 1.2.1.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject geonames "0.5.1-SNAPSHOT"
   :description "Clojure API for GeoNames."
   :url "http://github.com/r0man/geonames-clj"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
