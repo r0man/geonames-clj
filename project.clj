e53c8d5b6e572892dcbc79ed61fcf341b38d8e9c
Roman <roman@burningswell.com>
2011-09-24 11:20:58 +0200

    Release 0.6.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject geonames "0.6.0-SNAPSHOT"
+(defproject geonames "0.6.0"
   :description "Clojure API for GeoNames."
   :url "http://github.com/r0man/geonames-clj"
   :dependencies [[clj-http "0.2.0"]
-                 [org.clojure/clojure "1.3.0-RC0"]
+                 [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]]
   :multi-deps {"1.2.1" [[clj-http "0.2.0"]
                         [org.clojure/clojure "1.2.1"]
