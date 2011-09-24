06fa513cb433ed2ba20f5b3eeaf7a4e76e4ecdd7
Roman <roman@burningswell.com>
2011-09-24 11:21:36 +0200

    Started 0.6.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject geonames "0.6.0"
+(defproject geonames "0.6.1-SNAPSHOT"
   :description "Clojure API for GeoNames."
   :url "http://github.com/r0man/geonames-clj"
   :dependencies [[clj-http "0.2.0"]
