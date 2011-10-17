cdef73236d6f72e987ca9bc919cbcd89b3d90e92
Roman <roman@burningswell.com>
2011-10-17 09:12:24 +0200

    Removed multi deps and upgraded clj-http.

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject geonames "0.6.1-SNAPSHOT"
   :description "Clojure API for GeoNames."
   :url "http://github.com/r0man/geonames-clj"
-  :dependencies [[clj-http "0.2.0"]
+  :dependencies [[clj-http "0.2.1"]
                  [org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]]
-  :multi-deps {"1.2.1" [[clj-http "0.2.0"]
-                        [org.clojure/clojure "1.2.1"]
-                        [org.clojure/data.json "0.1.1"]]})
+                 [org.clojure/data.json "0.1.2"]])
