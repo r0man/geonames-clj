2026213019dc476d1456964b2284eb2852d939c4
Roman <roman@burningswell.com>
2011-12-27 11:03:47 +0100

    Switched to clj-http 0.2.6.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geonames "0.6.1-SNAPSHOT"
   :description "Clojure API for GeoNames."
   :url "http://github.com/r0man/geonames-clj"
-  :dependencies [[clj-http "0.2.1"]
+  :dependencies [[clj-http "0.2.6"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]])
