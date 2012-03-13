0cc4b20766832ec863ee6ac2c595a3ab4a39c1ed
Roman <roman@burningswell.com>
2012-03-13 21:34:09 +0100

    Get ready for Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject geonames "0.6.1-SNAPSHOT"
-  :description "Clojure API for GeoNames."
+(defproject geonames/geonames "0.6.1-SNAPSHOT"
   :url "http://github.com/r0man/geonames-clj"
+  :description "Clojure API for GeoNames."
+  :author "Roman Scherer"
   :dependencies [[clj-http "0.2.7"]
                  [org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.2"]])
+                 [org.clojure/data.json "0.1.2"]]
+  :min-lein-version "2.0.0")
