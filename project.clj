0e8226c30e91d9ffb2999268717b1c15c9332a76
Roman Scherer <roman.scherer@burningswell.com>
2013-01-22 21:31:12 +0100

    Release 0.6.3.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject geonames/geonames "0.6.3-SNAPSHOT"
+(defproject geonames/geonames "0.6.3"
   :description "Clojure API for GeoNames."
   :min-lein-version "2.0.0"
   :url "http://github.com/r0man/geonames-clj"
   :author "Roman Scherer"
-  :dependencies [[clj-http "0.5.6"]
+  :dependencies [[clj-http "0.6.3"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.0"]])
+                 [org.clojure/data.json "0.2.1"]])
