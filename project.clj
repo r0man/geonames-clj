d8dd0083f3018636aeeee81f26e7ff24a43f4314
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:15:07 +0100

    Added licence and url.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
 (defproject geonames/geonames "0.6.4"
   :description "Clojure API for GeoNames."
+  :url "https://github.com/r0man/geonames-clj"
   :min-lein-version "2.0.0"
   :url "http://github.com/r0man/geonames-clj"
   :author "Roman Scherer"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.6.4"]
                  [org.clojure/clojure "1.5.0"]
                  [org.clojure/data.json "0.2.1"]])
