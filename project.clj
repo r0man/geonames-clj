ecd7bdaf676d8c9c80b0cfff50a684229c9b2fc1
Roman Scherer <roman.scherer@burningswell.com>
2014-03-28 17:28:33 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[clj-http "0.7.7"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.3"]])
+  :dependencies [[clj-http "0.9.1"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.4"]])
