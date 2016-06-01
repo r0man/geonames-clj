d5767391dffa99e4544b085673f41d44c5b80a11
Roman Scherer <roman@burningswell.com>
2016-06-01 22:56:15 +0200

    Update dependencies

--- README.md
+++ README.md
@@ -64,6 +64,6 @@ Via Clojars: http://clojars.org/geonames
 
 ## License
 
-Copyright (C) 2013-2014 Roman Scherer
+Copyright (C) 2013-2014 r0man
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Clojure API for GeoNames."
   :url "https://github.com/r0man/geonames-clj"
   :min-lein-version "2.0.0"
-  :author "Roman Scherer"
+  :author "r0man"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
-  :dependencies [[clj-http "0.9.1"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.4"]])
+  :dependencies [[clj-http "3.1.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]])
